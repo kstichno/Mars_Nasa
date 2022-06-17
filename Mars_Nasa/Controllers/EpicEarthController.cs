3d661d2881e1a8391e58f3b07dab7afb889ad92d
Rusonmed <104440319+Rusonmed@users.noreply.github.com>
2022-06-16 21:27:13 -0400

    Added EpicEarth Tab

---
+++ Mars_Nasa/Controllers/EpicEarthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsNasa.Controllers
+{
+    public class EpicEarthController : Controller
+    {
+        public IActionResult EpicEarth()
+        {
+            return View();
+        }
+    }
+}
